f78caf7c955116d690660ac157804a57820f1810
Stefan Feurle <stefan.feurle@t-systems.com>
2018-09-13 13:41:37 +0200

    added some methods to ipsec ext

--- lib/fgt_rest_api/ext/ipsec.rb
+++ lib/fgt_rest_api/ext/ipsec.rb
@@ -7,5 +7,26 @@ module FGT
         cmdb_get(path: 'vpn.ipsec', name: name.tr('_', '-'), vdom: vdom).results
       end
     end
+
+    def ipsec_tunnels(tunnel_name = nil)
+      options = tunnel_name.yield_self { |t| t.nil? ? {} : { tunnel: tunnel_name } }
+      monitor_get('vpn/ipsec', options).results.uniq
+    end
+
+    def ipsec_tunnel_names
+       ipsec_tunnels.select { |t1| t1['parent'].nil? }.map { |t| t['name'] }
+    end
+
+    def ipsec_tunnel_children
+       ipsec_tunnels.select { |t1| not(t1['parent'].nil?) }.map { |t| t['name'] }
+    end
+
+    def ipsec_incoming_bytes(tunnel_parent)
+       ipsec_tunnels.select { |t| t['parent'] == tunnel_parent || t['name'] == tunnel_parent }.map { |t2| t2['incoming_bytes'] }.inject(0){|sum,x| sum + x }
+    end
+
+    def ipsec_outgoing_bytes(tunnel_parent)
+       ipsec_tunnels.select { |t| t['parent'] == tunnel_parent || t['name'] == tunnel_parent }.map { |t2| t2['outgoing_bytes'] }.inject(0){|sum,x| sum + x }
+    end
   end
 end
